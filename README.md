27ee5309259f3b3fd3d601d9241adedfef24d16f
Bilal Güler <guleerbilal@gmail.com>
2022-08-10 00:22:21 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# MERHABA PROGRAMIMA HOŞ GELDİNİZ
+
+* [**Patika Profil Linkim**](https://app.patika.dev/guleerbilal)
+
+* Bu programımda kullanıcıdan alınan üssü alınacak sayı ve üssü olacak sayıyı hesaplıyor.
+
+## *Kodlarım*
+
+*![Kodlarım](img/kodlar.PNG)
+
+*![Output](img/output.PNG)
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int sayi, us = 1, sonuc = 1;
+
+        System.out.print("Üssü alonacak sayı: ");
+        sayi = scan.nextInt();
+        System.out.print("Üs olacak sayı: ");
+        us = scan.nextInt();
+
+        for (int i = 1; i <= us; i++) {
+            sonuc *= sayi;
+        }
+        System.out.println(sayi + " sayısının " + us + ". üssü: " + sonuc);
+    }
+}
